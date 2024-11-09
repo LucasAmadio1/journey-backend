9efc5fe678db0ace8d7b84064c39aedd70121b80
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-11-09 17:22:02 -0300

    route confirm participant

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 
 import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
