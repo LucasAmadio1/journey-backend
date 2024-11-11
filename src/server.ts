ba340888e70e23709ab9d4e69aea030e0f8ea0dc
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-11-10 22:59:51 -0300

    finishing routes

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,18 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 
-import { confirmTrip } from "./routes/confirm-trip";
-import { createTrip } from "./routes/create-trip";
-import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/activities/create-activity";
 import { getActivities } from "./routes/activities/get-activities";
 import { createLink } from "./routes/links/create-link";
 import { getLinks } from "./routes/links/get-links";
+import { updateTrip } from "./routes/trips/update-trip";
+import { createInvite } from "./routes/participants/create-invite";
+import { getParticipants } from "./routes/participants/get-participants";
+import { createTrip } from "./routes/trips/create-trip";
+import { confirmTrip } from "./routes/trips/confirm-trip";
+import { confirmParticipant } from "./routes/trips/confirm-participant";
+import { getTripDetails } from "./routes/trips/get-trip-details";
+import { getParticipant } from "./routes/participants/get-participant";
 
 const app = fastify();
 
@@ -29,6 +34,11 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDetails);
+app.register(getParticipant);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
